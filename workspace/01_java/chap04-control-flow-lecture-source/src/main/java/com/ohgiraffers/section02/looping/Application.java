efdfb45413edf3a690902e896a3668d796c90a73
moomint8 <office05053@gmail.com>
2024-01-15 11:16:27 +0900

    feat(looping) : 단독 for문 / 중첩 for문
    for문을 단독으로 사용할 때의 예제와 중첩해 사용할 때의 예제입니다.

--- workspace/01_java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application.java
+++ workspace/01_java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application.java
@@ -1,16 +1,27 @@
 package com.ohgiraffers.section02.looping;
 
 public class Application {
+
     public static void main(String[] args) {
+        A_for a_for = new A_for();
 
-        A_for aClass = new A_for();
+        /*필기. 단독 for 문 흐름 확인용 메소드 호출*/
+//        a_for.testSimpleForStatement();
+        /*필기. 반복문 사용하는 예제(반복문의 중요성)*/
+//        a_for.testForExample1();
+        /*필기. */
+//        a_for.testForExample2();
 
-        /* 필기. 단독 for문 흐름 확인용 메소드 호출 */
-//        aClass.testSimpleForStatement();
+        B_nestedFor bClass = new  B_nestedFor();
 
-        /* 필기. 반복문을 사용하는 예제(반복문 필요성) */
-//        aClass.testForExample1();
-        aClass.testForExample2();
+        /* 필기. 중첩 반복문을 사용하는 예제 */
+        bClass.printGugudanFromTwoToNine();
+        bClass.printUpgradeGugudanFromTwoToNine();
 
+        /* 필기. 중첩 반복문을 활용한 별찍기 관련 예제 */
+        bClass.printStars();
+
+        /* 필기. 중첩 반복문을 활용한 마름모 찍기 예제 */
+        bClass.printDiamond();
     }
 }
