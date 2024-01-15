b9d6616fa17de6404614fcd58e98ea02f9d42696
moomint8 <office05053@gmail.com>
2024-01-15 10:18:43 +0900

    feat(java) : base
    교육을 들으며 작성한 java 코드입니다. 추후 코드들은 기능단위로 제대로 커밋될 예정입니다.

---
+++ workspace/01_java/chap04-control-flow-lecture-source/src/main/java/com/ohgiraffers/section02/looping/Application.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.section02.looping;
+
+public class Application {
+    public static void main(String[] args) {
+
+        A_for aClass = new A_for();
+
+        /* 필기. 단독 for문 흐름 확인용 메소드 호출 */
+//        aClass.testSimpleForStatement();
+
+        /* 필기. 반복문을 사용하는 예제(반복문 필요성) */
+//        aClass.testForExample1();
+        aClass.testForExample2();
+
+    }
+}
