c36dcf55610b91f6a6af64d66810a64bcdc675cf
moomint8 <office05053@gmail.com>
2024-01-17 16:23:55 +0900

    docs(toy_project/GameThreeSixNine) : 369게임 폴더 위치 변경
    369게임 폴더 위치가 변경되었습니다.

--- workspace/toy_project/GameThreeSixNine/Application.java
+++ workspace/toy_project/GameThreeSixNine/Application.java
@@ -3,7 +3,6 @@ package com.ohgiraffers.toy;
 import com.ohgiraffers.toy.domain.ChooseToy;
 
 public class Application {
-
     public static void main(String[] args) {
         ChooseToy toy = new ChooseToy();
         toy.chooseToy();
