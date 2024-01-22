de3e0d78aad1dfead8abed00658fa6e8e2293f7a
moomint8 <office05053@gmail.com>
2024-01-22 14:08:18 +0900

    feat(generics) : generic, extends
    extends 키워드를 사용하여 특정 타입만 제네릭 타입으로 사용하도록 제한할 수 있다.

---
+++ workspace/01_java/chap10-generics-lecture-source/src/main/java/com/ohgiraffers/section02/extend/vo/DrunkenBunny.java
@@ -0,0 +1,9 @@
+package com.ohgiraffers.section02.extend.vo;
+
+public class DrunkenBunny extends Rabbit {
+
+    @Override
+    public void cry() {
+        System.out.println("봐니봐니 봐니뽜니 당근! 당그웨에에엑");
+    }
+}
