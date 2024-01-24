1bc1fbfc6f4236c2e9bb7b478b85aa794f802fb8
moomint8 <office05053@gmail.com>
2024-01-24 10:42:37 +0900

    feat(exception) : 사용자 정의 exception
    사용자 정의의 예외 클래스 정의 후 발생한 사용자 정의의 예외를 처리할 수 있다.
    finally를 활용한 try-catch문 이해하고 활용할 수 있다.
    multi-catch 구문을 이해하고 활용할 수 있다.

---
+++ workspace/01_java/chap12-exception-lecture-source/src/main/java/com/ohgiraffers/section02/userexception/ExceptionTest.java
@@ -0,0 +1,23 @@
+package com.ohgiraffers.section02.userexception;
+
+import com.ohgiraffers.section02.userexception.exception.*;
+
+public class ExceptionTest {
+    public void checkEnoughMoney(int price, int money)
+//            throws PriceNegaticeException, NotEnoughMoneyException, MoneyNegaticeException {
+            throws Exception {
+
+        /* 설명. throw를 통한 예외 발생 시 바로 메소드를 호출한 곳으로 예외 인스턴스와 함께 반환된다. */
+        if (price < 0) {
+            throw new PriceNegaticeException("상품 가격이 음수일 수 없습니다.");
+        }
+        if (money < 0) {
+            throw new MoneyNegaticeException("소지금이 음수일 수 없습니다.");
+        }
+        if (money < price) {
+            throw new NotEnoughMoneyException("소지금보다 상품 가격이 더 비쌉니다.");
+        }
+
+        System.out.println("가진 돈이 충분하시군요! 즐거운 쇼핑 되시길 바랍니다 고객님!");
+    }
+}
---
+++ workspace/01_java/chap12-exception-lecture-source/src/main/java/com/ohgiraffers/section02/userexception/exception/MoneyNegaticeException.java
@@ -0,0 +1,7 @@
+package com.ohgiraffers.section02.userexception.exception;
+
+public class MoneyNegaticeException extends Exception {
+    public MoneyNegaticeException(String message) {
+        super(message);
+    }
+}
