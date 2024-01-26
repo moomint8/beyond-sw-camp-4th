87735fba9a91b1031531f1f01bd5b448b2598f67
moomint8 <office05053@gmail.com>
2024-01-26 12:14:01 +0900

    feat(lambda) : 람다 기본 소개
    람다식에 대해 이해하고 활용할 수 있다.(feat.FunctionalInterface)
    람다식 활용을 위한 내부 인터페이스 관리 기법에 대해 이해할 수 있다.

---
+++ workspace/01_java/chap-15-lambda-lecture-source/src/main/java/com/ohgiraffers/section01/intro/CalculatorImpl.java
@@ -0,0 +1,8 @@
+package com.ohgiraffers.section01.intro;
+
+public class CalculatorImpl implements Calculator {
+    @Override
+    public int sumTwoNumber(int firstNum, int secondNum) {
+        return firstNum + secondNum;
+    }
+}
