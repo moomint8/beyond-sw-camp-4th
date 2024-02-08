a4536584eb21fcd5b7f993b30c2772a7dd0e4b43
moomint8 <office05053@gmail.com>
2024-02-08 14:27:46 +0900

    feat(Servlet) : Listener

---
+++ workspace/03_servlet/chap12-listener-lecture-source/src/main/java/com/ohgiraffers/section02/sessionListener/UserDTO.java
@@ -0,0 +1,51 @@
+package com.ohgiraffers.section02.sessionListener;
+
+import jakarta.servlet.http.HttpSessionBindingEvent;
+import jakarta.servlet.http.HttpSessionBindingListener;
+
+public class UserDTO implements HttpSessionBindingListener {
+    private String name;
+    private int age;
+
+    public UserDTO() {
+    }
+
+    public UserDTO(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDTO{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public void valueBound(HttpSessionBindingEvent event) {
+        System.out.println("UserDTOrk ekarla");
+    }
+
+    @Override
+    public void valueUnbound(HttpSessionBindingEvent event) {
+        System.out.println("UserDTO 제거됨");
+    }
+}
