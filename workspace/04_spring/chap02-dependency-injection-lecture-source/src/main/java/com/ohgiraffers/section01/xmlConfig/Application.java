cf864ca7de92d75e4e7b0bd53f159389ded2471e
moomint8 <office05053@gmail.com>
2024-02-13 12:22:10 +0900

    Spring(Dependency Injection) : xmlConfig

---
+++ workspace/04_spring/chap02-dependency-injection-lecture-source/src/main/java/com/ohgiraffers/section01/xmlConfig/Application.java
@@ -0,0 +1,26 @@
+package com.ohgiraffers.section01.xmlConfig;
+
+import com.ohgiraffers.common.MemberDTO;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext context
+                = new GenericXmlApplicationContext("section01/xmlConfig/spring-context.xml");
+
+        MemberDTO member = context.getBean(MemberDTO.class);
+
+        /* 설명. 컨테이너가 관리중인 MemberDTO타입의 은행코드 20번인 PersonalAccount 객체가 주입되어 있음을 확인 */
+        System.out.println(member.getPersnalAccount());
+
+        /* 설명. member bean이 가진 계좌 객체의 기능을 활용해 입금 및 출금을 진행해보자. */
+        System.out.println(member.getPersnalAccount().depository(10000));
+
+        System.out.println(member.getPersnalAccount().getBalance());
+
+        System.out.println(member.getPersnalAccount().withDraw(5000));
+
+        System.out.println(member.getPersnalAccount().getBalance());
+    }
+}
