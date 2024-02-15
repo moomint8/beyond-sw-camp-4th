200e3327343f0f4895724867fd6935e768e1d562
moomint8 <office05053@gmail.com>
2024-02-15 12:37:10 +0900

    feat(SpringBoot): Auto Configuration

---
+++ workspace/05_springBoot/chap01-auto-configuration-lecture-source/src/main/java/com/ohgiraffers/autoConfiguration/SpringConfiguration.java
@@ -0,0 +1,38 @@
+package com.ohgiraffers.autoConfiguration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class SpringConfiguration {
+
+    @Value("${test.value}")
+    public String value;
+
+    @Value("${test.age}")
+    public int age;
+
+    @Value("${username}")
+    public String name;
+
+    @Value("${test.array}")
+//    public List<String> list;
+    public String[] arr;
+
+    @Bean
+    public Object propertyReadTest() {
+        System.out.println("value = " + value);
+        System.out.println("age = " + age);
+        System.out.println("name = " + name);
+
+        System.out.println("=============== 설정에서 읽어들인 여러 값 처리 ===============");
+//        list.forEach(System.out::println);
+        Arrays.stream(arr).forEach(System.out::println);
+
+        return new Object();
+    }
+}
