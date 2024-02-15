fec2b213c8228f65358eb2e3aa633efb61b5dc8f
moomint8 <office05053@gmail.com>
2024-02-15 15:08:07 +0900

    style(SpringBoot): Request Mapping

--- workspace/05_springBoot/chap02-request-mapping-lecture-source/src/main/java/com/ohgiraffers/requestMapping/MethodMappingTestController.java
+++ workspace/05_springBoot/chap02-request-mapping-lecture-source/src/main/java/com/ohgiraffers/requestMapping/MethodMappingTestController.java
@@ -18,7 +18,7 @@ public class MethodMappingTestController {
 
         model.addAttribute("message", "신규 메뉴 등록용 핸들러 메소드 호출함...");
 
-        /* 설명.  */
+        /* 설명. 핸들러 메소드에서 반환하는 String 값은 templates에 있는 view(html 파일)의 이름이다. */
         return "mappingResult";
     }
 
