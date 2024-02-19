17a2a5cf0f70b83f8cfaaeb2ffa7789f7d014b6b
moomint8 <office05053@gmail.com>
2024-02-19 10:20:41 +0900

    feat(SpringBoot): Interceptor

---
+++ workspace/05_springBoot/chap06-interceptor-lecture-source/src/main/java/com/ohgiraffers/interceptor/MainController.java
@@ -0,0 +1,20 @@
+package com.ohgiraffers.interceptor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String defaultLocation() {
+        return "main";
+    }
+
+    @RequestMapping("/main")
+    public String main() {
+        return "main";
+    }
+
+
+}
