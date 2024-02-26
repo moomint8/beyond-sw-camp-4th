fddd89b278afb2d29af784687c24542a2e2c9b64
moomint8 <office05053@gmail.com>
2024-02-26 09:33:28 +0900

    feat(Mybatis Spring): Transactional

---
+++ workspace/07_mybatis_spring/chap02-transactional-lecture-source/src/main/java/com/ohgiraffers/transactional/section01/annotation/OrderMenu.java
@@ -0,0 +1,46 @@
+package com.ohgiraffers.transactional.section01.annotation;
+
+public class OrderMenu {
+    private int menuCode;
+    private int orderCode;
+    private int orderAmount;
+
+    public OrderMenu() {
+    }
+
+    public OrderMenu(int menuCode, int orderAmount) {
+        this.menuCode = menuCode;
+        this.orderAmount = orderAmount;
+    }
+
+    public OrderMenu(int menuCode, int orderCode, int orderAmount) {
+        this.menuCode = menuCode;
+        this.orderCode = orderCode;
+        this.orderAmount = orderAmount;
+    }
+
+    public int getMenuCode() {
+        return menuCode;
+    }
+
+    public int getOrderCode() {
+        return orderCode;
+    }
+
+    public int getOrderAmount() {
+        return orderAmount;
+    }
+
+    public void setOrderCode(int orderCode) {
+        this.orderCode = orderCode;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderMenu{" +
+                "menuCode=" + menuCode +
+                ", orderCode=" + orderCode +
+                ", orderAmount=" + orderAmount +
+                '}';
+    }
+}
