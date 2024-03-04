57d694e7c784174bc09c0cdc9f10ebb4e1c0e64d
moomint8 <office05053@gmail.com>
2024-03-04 12:31:41 +0900

    feat(MSA): Gateway, Load Balancing

---
+++ workspace/09_MSA/chap03-spring-cloud-gateway-lecture-lecture-source/src/main/java/org/teamone/gateway/Chap03SpringCloudGatewayLectureLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package org.teamone.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap03SpringCloudGatewayLectureLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap03SpringCloudGatewayLectureLectureSourceApplication.class, args);
+    }
+
+}
