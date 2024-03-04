351e466b6713f95e357595cc93f89a1fe332e3ea
moomint8 <office05053@gmail.com>
2024-03-04 09:49:13 +0900

    feat(MSA): eureka 기본 구축

---
+++ workspace/09_MSA/chap01-02-eureka-client-lecture-source/src/main/java/com/ohgiraffers/eurekaclient/Chap0102EurekaClientLectureSourceApplication.java
@@ -0,0 +1,16 @@
+package com.ohgiraffers.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/* 설명. 유레카 서버의 클라이언트로 등록할 프로젝트로 기동 */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Chap0102EurekaClientLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap0102EurekaClientLectureSourceApplication.class, args);
+    }
+
+}
