02ccf3034f37b9223b0bcc457bd5a04edc0261a6
moomint8 <office05053@gmail.com>
2024-04-02 09:24:40 +0900

    feat(vue): router

---
+++ workspace/13_vue/08_router/src/views/01_router/QueryString.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>QueryString</h1>
+        <h2>currentRoute = {{ currentRoute.query }}</h2>
+        <h2>name: {{ currentRoute.query.name }}</h2>
+        <h2>age: {{ currentRoute.query.age }}</h2>
+    </div>
+</template>
+
+<script setup>
+    import {useRoute} from 'vue-router'
+
+    const currentRoute = useRoute();
+</script>
+
+<style scoped>
+
+</style>
---
+++ workspace/13_vue/08_router/src/views/02_nestedRoute/RootRouter.vue
@@ -0,0 +1,8 @@
+<template>
+    <h1>RootRouter</h1>
+    <RouterView/>
+</template>
+
+<script setup>
+    import {RouterView} from 'vue-router'
+</script>
