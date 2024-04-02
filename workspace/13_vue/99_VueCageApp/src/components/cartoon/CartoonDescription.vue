5bc871c2496f8fb9afcc11361023b98efa072dd6
moomint8 <office05053@gmail.com>
2024-04-02 09:27:16 +0900

    feat(vue): vue cage app

---
+++ workspace/13_vue/99_VueCageApp/src/components/cartoon/CartoonDescription.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <div class="description">
+        <p>제작자 : <slot name="creator"></slot></p>
+        <p>제작년도 : <slot name="year"></slot></p>
+        <p>장르 : <slot name="genre"> </slot></p>
+    </div>
+</template>
+
+
+<style scoped>
+.description{
+    border: 1px dotted;
+    align-items: center;
+    align-content: center;
+    margin-top: 3px;
+    margin-bottom: 3px;
+}
+</style>
