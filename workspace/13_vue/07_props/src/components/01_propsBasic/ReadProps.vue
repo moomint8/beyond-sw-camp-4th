aba4bd0bf38e3913f33d64301fc59a2bf1088021
moomint8 <office05053@gmail.com>
2024-04-02 09:24:30 +0900

    feat(vue): props

---
+++ workspace/13_vue/07_props/src/components/01_propsBasic/ReadProps.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1> {{ props.readValue }}</h1>
+    자식의 readValue: <input v-model="props.readValue"/>
+</template>
+
+<script setup>
+    import {defineProps} from 'vue';
+    const props = defineProps({
+        readValue: String
+    });
+</script>
---
+++ workspace/13_vue/07_props/src/components/02_propsDrilling/DrillingChild.vue
@@ -0,0 +1,11 @@
+<template>
+    <h2>child : {{ injectCount }}</h2>
+    <GrandChild/>
+</template>
+
+<script setup>
+    import {inject} from 'vue';
+    import GrandChild from './DrillingGrandChild.vue';
+
+    const injectCount = inject('count');
+</script>
